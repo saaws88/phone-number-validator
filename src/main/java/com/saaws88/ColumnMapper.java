e92981e8451b4ef7f9de2f914a967afffef640ca
saaws88 <v.zibnitsky@yandex.ru>
2024-03-20 05:39:34 +0700

    Rewrote parser to work with xls files using Apache POI library

---
+++ src/main/java/com/saaws88/ColumnMapper.java
@@ -0,0 +1,11 @@
+package com.saaws88;
+
+public class ColumnMapper {
+
+  int ORDER_NUMBER = 0;
+  int ORDER_STATUS = 1;
+  int IS_DELETED = 2;
+  int RECEIVER_PHONES = 3;
+  int PAYER = 4;
+
+}
--- src/main/java/com/saaws88/Main.java
+++ src/main/java/com/saaws88/Main.java
@@ -1,21 +1,26 @@
 package com.saaws88;
 
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
 
-    Parser p = new Parser();
+    XlsParser p = new XlsParser();
 
-    for (String arg : args) {
-        long startTime = System.nanoTime();
-        p.writeWrongNumbersToFile(p.getWrongNums(arg));
-        long endTime = System.nanoTime();
-        double elapsedTime = (endTime - startTime) / 1e+9;
+    long startTime = System.nanoTime();
+    HSSFWorkbook wbIn = p.readWorkbook(args[0]);
+    List<HSSFRow> wrongNumbers = p.getWrongNumberRowsList(wbIn);
+    HSSFWorkbook wbOut = p.writeToWorkBook(wrongNumbers);
+    p.writeToFile(wbOut);
+    long endTime = System.nanoTime();
+    double elapsedTime = (endTime - startTime) / 1e+9;
+    System.out.printf("Выполнено за %.2f сек. \n", elapsedTime);
 
-        System.out.printf("Выполнено за %.2f сек. \n", elapsedTime);
-    }
   }
 
 }
